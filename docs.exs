5cfb71d316bd358edd4ddc78b0c56e8d66ad380c
Pedro Cavalcante <pedrocolrj@gmail.com>
2020-09-04 00:51:44 -0300

    mmm

---
+++ docs.exs
@@ -0,0 +1,15 @@
+## Tipos
+
+1 # integer
+1.0 # float
+:true # booleano
+:atom # um átomo
+"elixir" # uma string
+[1, 2, 3] # uma lista
+{1, 2, 3} # uma tupla
+
+## Aritmética
+
+1 + 2
+5 * 5
+10 / 2
